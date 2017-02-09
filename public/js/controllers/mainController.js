d4b02672fb8e6ae477d47d14fb33b7f3b5f98070
jucieny <jucieny.santos@f1solucoes.com.br>
2017-02-09 19:49:14 -0200

    Iniciado a programação das controllers do angular

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -6,12 +6,12 @@ var app = angular.module('portfolio', []);
 
 app.controller('mainController', [ '$rootScope', '$scope', '$location', '$localStorage', 'Main', function($rootScope, $scope, $location, $localStorage, Main){
 	
-	$scope.signin 	= function(){
+	$scope.login 	= function(){
 		
 	};
-	$scope.signout 	= function(){};
-	$scope.myaccout = function(){};
-	$scope.logout 	= function(){};
+	$scope.logout 	= function(){
+		
+	};
 
 	$scope.token = $localStorage.token;
 
---
+++ public/js/services/authenticate.service.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/* controller */
+	angular
+		.module('portfolio')
+		.factory('AuthenticationService', Service);
+
+
+	function Service($http, $localStorage){
+
+		var service 	= {};
+
+		service.Login 	= Login;
+		service.Logout 	= Logout;
+
+		return service;
+
+		function Login( username, password, callback ){
+			$http.post('/login/authenticate', { name: username, password: password})
+				.success(function(res){
+					if (res.token){
+						$localStorage.currentUser = { name: username, token: res.token };
+						$http.defaults.headers.common.Authorization = res.token;
+						callback(true);
+					} else {
+						callback(true);
+					}
+				});
+		}
+
+		function Logout() {
+			delete $localStorage.currentUser;
+			$http.defaults.headers.common.Authorization = '';
+		}
+
+	}
